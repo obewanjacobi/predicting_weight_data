c8bbf7c024a38c4288a79e13318a432443f08b1e
Jacob Townson <obewanjacobi@aim.com>
2015-08-13 12:42:50 -0400

    First Try

--- Machine_Learning_Project_Script.R
+++ Machine_Learning_Project_Script.R
@@ -4,6 +4,8 @@
 # John Hopkins Machine Learning Course
 #-----------------------------------------
 
+library(caret)
+
 # This function writes the text files containing the answers I have for the project.
 
 pml_write_files = function(x){
@@ -25,6 +27,40 @@ if(exists('test') == FALSE){
 
 # Clean the Data
 
+goodNames <- sapply(test, function(x)!all(is.na(x)))
+df <- train[,goodNames]
+namesdf <- names(df)
+wanted <- namesdf[-c(1,3,4,5,6,7)]
+df <- df[,wanted]
+
+
+# Make Data Partition
+
+set.seed(2200)
+separate <- createDataPartition(y = df$classe, p = .6, list = FALSE)
+trainData <- df[separate,]
+testData <- df[-separate,]
+
+
+# Make the Trees
+
+mod <- train(x = trainData[, -54], y = trainData$classe,
+             method = 'rf', trControl = trainControl(method = 'oob'),
+             importance = TRUE, ntree = 100, tuneLength = 3, do.trace = 50)
+mypredicts <- predict(mod, newdata = testData[,1:53])
+confusionMatrix(mypredicts, testData$classe)
+myImp <- varImp(mod, scale = FALSE, type = 1)
+plot(myImp, top = 10, main = 'Variable Importance')
+
+
+# Predict the Examination Data
+
+lastTest <- test[,goodNames]
+namesdf <- names(lastTest)
+wanted <- namesdf[-c(1,3,4,5,6,7)]
+lastTest <- lastTest[,wanted]
+myAnswer <- predict(mod, newdata = lastTest[,1:53])
+pml_write_files(myAnswer)
 
 
 
